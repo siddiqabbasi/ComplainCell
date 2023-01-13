6198029f6626634a3451f6725ca7005d2c4ebf62
Siddiq Abbasi <siddiqabbasi0@gmail.com>
2023-01-13 20:14:22 +0500

    Add files via upload

---
+++ react-laravel/src/components/home.js
@@ -0,0 +1,219 @@
+import { useState, useEffect } from "react";
+import AuthUser from "./AuthUser";
+
+export default function Home() {
+  const { http, setToken } = AuthUser();
+  const [complainant_name, setComplainantName] = useState(null);
+  const [complainant_mobile_no, setComplainantMobileNo] = useState(null);
+  const [crime_type, setCrimeType] = useState("mobile");
+  const [imei_no, setIMEINo] = useState(null);
+  const [registration_no, setRegistrationNo] = useState(null);
+  const [engine_no, setEngineNo] = useState(null);
+  const [chasis_no, setChasisNo] = useState(null);
+  const [isError, setError] = useState(false);
+  //   const [password, setPassword] = useState();
+
+  useEffect(() => {
+    // fetchAllUsers();
+  });
+
+  const fetchAllUsers = () => {
+    http.get("/users").then((res) => {
+      // console.log(res.data)
+    });
+  };
+
+  const submitForm = () => {
+    console.log(
+      complainant_name,
+      complainant_mobile_no,
+      crime_type,
+      imei_no,
+      registration_no,
+      engine_no,
+      chasis_no
+    );
+    const userData = complainant_name && complainant_mobile_no,
+      mobileData = crime_type == "mobile" && imei_no,
+      vehicleData =
+        crime_type == "vehicle" && registration_no && engine_no && chasis_no;
+    if ((userData && mobileData) || (userData && vehicleData)) {
+      let formData = new FormData();
+      formData.append("complainant_name", complainant_name);
+      formData.append("complainant_mobile_no", complainant_mobile_no);
+      formData.append("offence_date", null);
+      formData.append("offence_month", null);
+      formData.append("offence_year", null);
+      formData.append("complain_date", null);
+      formData.append("complain_month", null);
+      formData.append("crime_type", crime_type);
+      formData.append("imei_no", imei_no);
+      formData.append("make", null);
+      formData.append("model", null);
+      formData.append("color", null);
+      formData.append("registration_no", registration_no);
+      formData.append("engine_no", engine_no);
+      formData.append("chasis_no", chasis_no);
+      formData.append("police_station", null);
+      formData.append("district", null);
+      formData.append("location", null);
+      formData.append("incident_date", null);
+      formData.append("incident_time", null);
+      formData.append("status_crime_type", null);
+      formData.append("fir_no", null);
+      formData.append("date_of_fir", null);
+      formData.append("crime_status", null);
+      formData.append("file_path", null);
+      formData.append("user_id", null);
+
+      console.log("formData", [...formData]);
+      // api call
+      http
+        .post("/addcomplain", formData)
+        .then((res) => {
+          console.log(res.data);
+          // setToken(res.data.user,res.data.access_token);
+        })
+        .catch((err) => {
+          console.log(err, "dsafa");
+        });
+    } else {
+      setError(true);
+    }
+  };
+
+  return (
+    <div className="row justify-content-center pt-5">
+      <div className="col-sm-6">
+        <div className="card p-4">
+          <h1 className="text-center mb-3">Register your Complain here </h1>
+          <div className="form-group">
+            <label>Complainant Name:</label>
+            <input
+              type="name"
+              className="form-control"
+              placeholder="Enter your name here"
+              onChange={(e) => {
+                setComplainantName(e.target.value);
+                setError(false);
+              }}
+              id="complainant_name"
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Complainant Mobile No.:</label>
+            <input
+              type="number"
+              className="form-control"
+              placeholder="Enter you mobile number here"
+              onChange={(e) => {
+                setComplainantMobileNo(e.target.value);
+                setError(false);
+              }}
+              id="complainant_mobile_no"
+            />
+          </div>
+          <div
+            className="row justify-content-center"
+            style={{ marginTop: "20px" }}
+          >
+            <div
+              className="col-sm-5 border border-dark rounded"
+              onClick={() => {
+                setIMEINo(null);
+                setCrimeType("vehicle");
+              }}
+            >
+              <h5>Vehicle</h5>
+            </div>
+            <div
+              className="col-sm-5 border border-dark rounded"
+              onClick={() => {
+                setRegistrationNo(null);
+                setEngineNo(null);
+                setChasisNo(null);
+                setCrimeType("mobile");
+              }}
+            >
+              <h5>Mobile</h5>
+            </div>
+          </div>
+          {crime_type == "mobile" ? (
+            <div className="form-group mt-3">
+              <label>IMEI No.:</label>
+              <input
+                type="number"
+                className="form-control"
+                placeholder="Enter you IMEI number here"
+                onChange={(e) => {
+                  setIMEINo(e.target.value);
+                  setError(false);
+                }}
+                id="imei_no"
+              />
+            </div>
+          ) : null}
+          {crime_type == "vehicle" ? (
+            <div className="form-group mt-3">
+              <label>Registration No.:</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter you mobile Registration here"
+                onChange={(e) => {
+                  setRegistrationNo(e.target.value);
+                  setError(false);
+                }}
+                id="registration_no"
+              />
+            </div>
+          ) : null}
+          {crime_type == "vehicle" ? (
+            <div className="form-group mt-3">
+              <label>Engine No.:</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter you Engine number here"
+                onChange={(e) => {
+                  setEngineNo(e.target.value);
+                  setError(false);
+                }}
+                id="engine_no"
+              />
+            </div>
+          ) : null}
+          {crime_type == "vehicle" ? (
+            <div className="form-group mt-3">
+              <label>Chasis No.:</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter you Chasis number here"
+                onChange={(e) => {
+                  setChasisNo(e.target.value);
+                  setError(false);
+                }}
+                id="chasis_no"
+              />
+            </div>
+          ) : null}
+          {isError ? (
+            <p
+              style={{ alignSelf: "center", margin: "0px", marginTop: "10px" }}
+            >
+              All fields are required
+            </p>
+          ) : null}
+          <button
+            type="button"
+            onClick={submitForm}
+            className="btn btn-primary mt-4"
+          >
+            Register
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
